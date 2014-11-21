8cb4a3a966ddf91add2aad9389173641018d3bd5
Jeffrey Lyman <jlyman@macalester.edu>
2014-11-21 11:47:11 -0600

    Made a bitter jaded version of the website

---
+++ app.js
@@ -0,0 +1,59 @@
+var http = require('http');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extend: false}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')))
+
+var urlparser = bodyParser.urlencoded({extended: false});
+
+app.get('/buy', function(req,res){
+    res.render('buy', null)
+})
+
+app.get('/', function(req,res){
+    res.render('index', null)
+})
+
+app.get('/about', function(req,res){
+    res.render('about', null)
+})
+
+app.get('/technique', function(req,res){
+    res.render('technique', null)
+})
+
+app.get('/store', function(req,res){
+    res.render('store', null)
+})
+
+app.get('/gallery', function(req,res){
+    res.render('gallery', null)
+})
+
+app.get('/contact', function(req,res){
+    res.render('contact', null)
+})
+
+app.get('/commission', function(req,res){
+    res.render('commission', null)
+})
+
+app.get('/admin', function(req,res){
+    res.render('admin', null)
+})
+
+server.listen(8080, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Server Listening on %s:%s", host, port);
+})
