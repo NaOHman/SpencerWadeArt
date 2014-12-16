2704dd77b247609f518a3836ff95450bed8d5ceb
Rebecca <rgold1@macalester.edu>
2014-12-15 19:50:37 -0600

    some fixes to payment

--- routes/routes.js
+++ routes/routes.js
@@ -167,20 +167,19 @@ module.exports = function(app, mongoskin, path, passport) {
    //Stripe payment
     app.post('/charge', function(req, res) {
         var stripeToken = req.body.stripeToken;
+        var stripeEmail = req.body.stripeEmail;
+        var amount = req.body.data-amount
         var charge = stripe.charges.create({
-            amount: 1000, // amount in cents, again
+            amount: amount, // amount in cents, again
             currency: "usd",
             card: stripeToken,
-            description: "payinguser@example.com"
+            description: stripeEmail 
         }, function(err, charge) {
             if (err && err.type === 'StripeCardError') {
                 // The card has been declined
             } else {
                 //Render a thank you page called "Charge"
-                myId = mongoskin.helper.toObjectID(fields.artId);
-                req.collections.art.update({_id: myId},{$set: {forSale: false}}, function(err, response){
-                    res.render('charge', { title: 'Charge' });
-                }
+                res.render('charge', { title: 'Charge' });
             }
         });
     });
@@ -406,7 +405,6 @@ function formToDB(fields){
     fields.dim = fields.width + 'x' + fields.height;
     fields.date = fields.year;
     fields.numdate = (fields.year*1000) + (monthToN(fields.month)*100) + fields.day;
-    fields.cents = 100 * fields.price;
     if (fields.depth != '')
            fields.dim = fields.dim +'x' + fields.depth;
     if (fields.month != '---'){
